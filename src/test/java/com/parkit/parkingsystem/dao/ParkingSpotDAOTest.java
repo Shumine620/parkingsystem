09e007fcf578348d1b2d0a8aac0be21074b5e69a
Shumine <jah2jah.sr@gmail.com>
2021-03-10 19:25:27 +0100

    ParkingSpotDAOTest passed

--- src/test/java/com/parkit/parkingsystem/dao/ParkingSpotDAOTest.java
+++ src/test/java/com/parkit/parkingsystem/dao/ParkingSpotDAOTest.java
@@ -1,39 +1,24 @@
 package com.parkit.parkingsystem.dao;
 
-import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.model.ParkingSpot;
-import com.parkit.parkingsystem.model.Ticket;
-import com.parkit.parkingsystem.util.InputReaderUtil;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 
 import java.io.IOException;
 
 import static com.parkit.parkingsystem.constants.ParkingType.CAR;
 import static org.junit.jupiter.api.Assertions.*;
-//import static org.assertj.core.api.Assertions.assertThat;
+
 
 
 class ParkingSpotDAOTest {
     private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
     private static DataBasePrepareService dataBasePrepareService;
     private static ParkingSpotDAO parkingSpotDAO;
-    private static TicketDAO ticketDAO;
-    private static Ticket ticket;
     private static ParkingSpot parkingSpot;
-    private static ParkingType parkingType;
-
-    @Mock
-    private static InputReaderUtil inputReaderUtil;
-    @Mock
-    private static final Logger logger = LogManager.getLogger("ParkingSpotDAO");
-
 
     @BeforeEach
     public void setUpPerTest() {
